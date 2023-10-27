a1e646470ca881b2b88898f3f122b71aff9aabe4
sahil-me <sahil.activedev@gmail.com>
2023-10-27 11:08:29 +0530

    animation

--- script.js
+++ script.js
@@ -33,10 +33,13 @@ Shery.makeMagnet(".navbar a" , {
 
   Shery.imageEffect(".s1img", {
     style: 1 ,
+    debug: false,
     config:{"a":{"value":2,"range":[0,30]},"b":{"value":0.7,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":1.3295454545454546},"gooey":{"value":false},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.002,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}}
   });
 
   Shery.imageEffect(".s2img", {
     style: 1 ,
-    debug: true,
+    debug: false,
   });
+
+  
